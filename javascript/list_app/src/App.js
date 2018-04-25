695b153e5b6fa8722ddfb9b6bfa0db6b3af7469a
Rogelio Zamudio <yeyozaz@gmail.com>
2018-04-25 01:17:24 -0500

    using fetch to grab data, mapped data to lis under a ul element

--- javascript/list_app/src/App.js
+++ javascript/list_app/src/App.js
@@ -3,18 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json")
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          quotes: data
+        })
+      })
+      .then(
+        console.log(this.state.quotes)
+      )
+  }
+
+
+
   render() {
+    const { quotes } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <ul>
+        {quotes.map((quote, index) => (
+          <li key={index}>
+            {quote.source}
+          </li>
+        ))}
+      </ul>
+    )
   }
 }
 
