313fd38e561034b3ec238ee76e09219243f77e58
Rogelio Zamudio <yeyozaz@gmail.com>
2018-04-26 14:20:45 -0500

    added game dealing methods and hand display methods

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -51,6 +51,53 @@ class Hand
   end
 end
 
+class Game
+  attr_accessor :player_hand, :dealer_hand, :deck
+  def initialize
+    @player_hand = Hand.new
+    @dealer_hand = Hand.new
+    @deck = Deck.new
+  end
+
+  def deal(player, deck)
+    player.cards.push(deck.deal_card)
+  end
+
+  def deal_starting_hand(player, dealer, deck)
+    2.times do
+      deal(player, deck)
+      deal(dealer, deck)
+    end
+  end
+
+  def display_player_hand
+    player_hand.cards.each do |card|
+      "#{card.name} of #{card.suite}"
+    end
+  end
+
+  def display_player_total
+    @total
+    player_hand.cards.each do |card|
+      @total += card.value
+    end
+    return @total
+  end
+
+  def print_player_cards
+    puts display_player_hand
+  end
+
+  def print_player_total
+    puts display_player_total
+  end
+
+  def bust
+
+  end
+
+end
+
 require 'test/unit'
 
 class CardTest < Test::Unit::TestCase
@@ -65,6 +112,7 @@ class CardTest < Test::Unit::TestCase
   def test_card_name_is_correct
     assert_equal @card.name, :ten
   end
+
   def test_card_value_is_correct
     assert_equal @card.value, 10
   end
@@ -88,4 +136,36 @@ class DeckTest < Test::Unit::TestCase
     @deck.shuffle
     assert_equal @deck.playable_cards.size, 52
   end
+end
+
+class GameTest < Test::Unit::TestCase
+  def setup
+    @game = Game.new
+  end
+
+  def test_game_has_a_deck
+    assert_equal @game.deck.playable_cards.size, 52
+  end
+
+  def test_game_has_a_player_hand
+    @game.deal(@game.player_hand, @game.deck)
+    @game.deal(@game.player_hand, @game.deck)
+    @cards = []
+    @cards << @game.player_hand.cards[0]
+    @cards << @game.player_hand.cards[1]
+
+    assert @game.display_player_hand, @card_string
+  end
+
+  def test_game_sums_player_card_values
+    @game.deal(@game.player_hand, @game.deck)
+    @game.deal(@game.player_hand, @game.deck)
+    @cards = []
+    @cards << @game.player_hand.cards[0]
+    @cards << @game.player_hand.cards[1]
+
+    @game.print_player_total
+
+  end
+
 end
